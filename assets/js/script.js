d3bbac38f1e3daabcc7ed3646542846037cad3e5
Nick Rutten <nick@sparrowinspace.com>
2014-11-03 11:15:19 +0100

    fix twitter feed display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
+  // Load Typekit fonts
+  try{Typekit.load();}catch(e){}
   
+  // Define variables for use in mobile navigation menu code
   var navMenu = $('nav > ul');
   var navButton = $('nav > button');
 
@@ -20,14 +23,11 @@ $(document).ready(function(){
   );
 
   // Twitterwidget, source: twitter.com
-  if(location.pathname === '/overmij.html') {
+  if(location.pathname === "/~ruttenn001/website/overmij.html") {
     /*  I added the following line to test if this if-statement works, 
         and I'm not taking it out */
     console.log('Yeah');
     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
   }
-
-  // Load Typekit fonts
-  try{Typekit.load();}catch(e){}
 
 });
